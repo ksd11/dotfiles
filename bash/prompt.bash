2e410b883c7e253eaf469d780d9a6d53672ad279
ksd1 <1614545312@qq.com>
2023-04-17 19:43:35 +0800

    small change suit myself

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,12 +1,12 @@
 ATTRIBUTE_BOLD='\[\e[1m\]'
 ATTRIBUTE_RESET='\[\e[0m\]'
 COLOR_DEFAULT='\[\e[39m\]'
-COLOR_RED='\[\e[31m\]'
-COLOR_GREEN='\[\e[32m\]'
-COLOR_YELLOW='\[\e[33m\]'
-COLOR_BLUE='\[\e[34m\]'
-COLOR_MAGENTA='\[\e[35m\]'
-COLOR_CYAN='\[\e[36m\]'
+COLOR_RED='\[\e[1;31m\]'
+COLOR_GREEN='\[\e[1;32m\]'
+COLOR_YELLOW='\[\e[1;33m\]'
+COLOR_BLUE='\[\e[1;34m\]'
+COLOR_MAGENTA='\[\e[1;35m\]'
+COLOR_CYAN='\[\e[1;36m\]'
 
 machine_name() {
     if [[ -f $HOME/.name ]]; then
@@ -17,7 +17,7 @@ machine_name() {
 }
 
 PROMPT_DIRTRIM=3
-PS1="\n${COLOR_BLUE}#${COLOR_DEFAULT} ${COLOR_CYAN}\\u${COLOR_DEFAULT} ${COLOR_GREEN}at${COLOR_DEFAULT} ${COLOR_MAGENTA}\$(machine_name)${COLOR_DEFAULT} ${COLOR_GREEN}in${COLOR_DEFAULT} ${COLOR_YELLOW}\w${COLOR_DEFAULT}\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_RED}!${COLOR_DEFAULT} \"; fi)${COLOR_BLUE}>${COLOR_DEFAULT} "
+PS1="${COLOR_BLUE}#${COLOR_DEFAULT} ${COLOR_CYAN}\\u${COLOR_DEFAULT} ${COLOR_GREEN}at${COLOR_DEFAULT} ${COLOR_MAGENTA}\$(machine_name)${COLOR_DEFAULT} ${COLOR_GREEN}in${COLOR_DEFAULT} ${COLOR_YELLOW}\w${COLOR_DEFAULT}\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_RED}!${COLOR_DEFAULT} \"; fi)${COLOR_BLUE}>${COLOR_DEFAULT} "
 PS2="${COLOR_BLUE}>${COLOR_DEFAULT} "
 
 COLOR_GRAY='\e[38;5;246m'
--- bash/settings.bash
+++ bash/settings.bash
@@ -3,4 +3,5 @@ HISTFILE="$HOME/.bash_history"
 SAVEHIST=$HISTSIZE
 shopt -s histappend # append to history file
 
-export EDITOR=vim
+# export EDITOR=vim
+
