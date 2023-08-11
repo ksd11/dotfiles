86c49b2ddb59357ccc9286be06e27a0edc5cfc48
ksd1 <1614545312@qq.com>
2023-08-11 22:01:09 +0800

    bash显示分支，关闭默认py虚拟环境

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -16,8 +16,25 @@ machine_name() {
     fi
 }
 
+function git_branch {
+    # 如果没有git直接退出
+    type git > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        return
+    fi
+
+    # 获取当前仓库的分支
+    branch="`git branch 2>/dev/null | grep "^\*" | sed -e "s/^\*\ //"`"
+    if [ "${branch}" != "" ];then
+        if [ "${branch}" = "(no branch)" ];then
+            branch="(`git rev-parse --short HEAD`...)"
+        fi
+        echo " ($branch)"
+    fi
+}
+
 PROMPT_DIRTRIM=3
-PS1="${COLOR_BLUE}#${COLOR_DEFAULT} ${COLOR_CYAN}\\u${COLOR_DEFAULT} ${COLOR_GREEN}at${COLOR_DEFAULT} ${COLOR_MAGENTA}\$(machine_name)${COLOR_DEFAULT} ${COLOR_GREEN}in${COLOR_DEFAULT} ${COLOR_YELLOW}\w${COLOR_DEFAULT}\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_RED}!${COLOR_DEFAULT} \"; fi)${COLOR_BLUE}>${COLOR_DEFAULT} "
+PS1="${COLOR_BLUE}#${COLOR_DEFAULT} ${COLOR_CYAN}\\u${COLOR_DEFAULT} ${COLOR_GREEN}at${COLOR_DEFAULT} ${COLOR_MAGENTA}\$(machine_name)${COLOR_DEFAULT} ${COLOR_GREEN}in${COLOR_DEFAULT} ${COLOR_YELLOW}\w${COLOR_DEFAULT} -> \$(git_branch)\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_RED}!${COLOR_DEFAULT} \"; fi)${COLOR_BLUE}>${COLOR_DEFAULT} "
 PS2="${COLOR_BLUE}>${COLOR_DEFAULT} "
 
 COLOR_GRAY='\e[38;5;246m'
